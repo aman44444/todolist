8387b2b6ed650b772f22b07d21f962a9cefd85e6
aman1357 <amancorp04@gmail.com>
2026-02-20 23:55:21 +0530

    feat: add task suggestions data array

---
+++ src/data/data.js
@@ -0,0 +1,10 @@
+export const TaskSuggestions = [
+    "Morning workout",
+  "Evening walk",
+  "Meditate for 10 minutes",
+  "Drink 2L water",
+  "Call parents",
+  "Pay electricity bill",
+  "Buy groceries",
+  "Clean room",
+]
