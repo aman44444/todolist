60da7eb3d60ff9766f89e751d7112b9a75adeda8
aman1357 <amancorp04@gmail.com>
2026-03-08 20:58:20 +0530

    feat : add reusable button component

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,21 @@
+const Button = ({
+  children,
+  onClick,
+  type = "button",
+  variant = "primary",
+  size = "md",
+  disabled = false,
+}) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className={`btn btn-${variant} btn-${size}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
