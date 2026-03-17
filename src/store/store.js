a12e643db176c92075798abf1b8e7427de9801b3
aman1357 <amancorp04@gmail.com>
2026-03-17 23:12:16 +0530

    feat:add global alarm state

--- src/store/store.js
+++ src/store/store.js
@@ -1,38 +1,51 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
-const useTodosStore = create(persist((set) => {
-    return { 
-       tasks: [],
+const useTodosStore = create(
+  persist(
+    (set) => {
+      return {
+        tasks: [],
+        mode: "normal",
+        activeAlarmTaskId: null,
 
-       mode: "normal", 
+        setMode: (newMode) => set({ mode: newMode }),
 
-       setMode: (newMode) => set({ mode: newMode }),
+        triggerAlarm: (taskId) =>
+          set({
+            activeAlarmTaskId: taskId,
+          }),
 
-       addTasks:(task) => {
-           set((state) => ({
-                tasks : [
-                  ...state.tasks,{
-                    id: Date.now().toString(),
-                    text: task.text,
-                    alarmTime: task.alarmTime || null,
-                    completed: false,
-                  }
-                ]
-           }))
-       },
-       removeTask:(id) =>
-           set((state)=>
-            ({
-                tasks:state.tasks.filter((task) => task.id !== id),
-         
-       }))
-    };
-},
-   {
-     name:"Todo-list",
-     storage:createJSONStorage(() => localStorage),
-   }
-)) 
+        stopAlarm: () =>
+          set({
+            activeAlarmTaskId: null,
+          }),
+        addTasks: (task) => {
+          set((state) => ({
+            tasks: [
+              ...state.tasks,
+              {
+                id: Date.now().toString(),
+                text: task.text,
+                alarmTime: task.alarmTime || null,
+                completed: false,
+              },
+            ],
+          }));
+        },
+        removeTask: (id) =>
+          set((state) => ({
+            tasks: state.tasks.filter((task) => task.id !== id),
+            activeAlarmTaskId: null, 
+          })),
+      
+      };
+    },
+    {
+      name: "Todo-list",
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+);
 
 export default useTodosStore;
