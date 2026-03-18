a9e1e681d605a286c40008c7e3b008890bc15481
aman1357 <amancorp04@gmail.com>
2026-03-18 23:44:42 +0530

    feat : add alarm engine

---
+++ src/services/alarmEngine.js
@@ -0,0 +1,30 @@
+export class AlarmEngine {
+  interval = null;
+
+  start(tasks, triggerAlarm) {
+    if (this.interval) return; 
+
+    this.interval = setInterval(() => {
+      const now = new Date();
+
+      const currentTime =
+        now.getHours().toString().padStart(2, "0") +
+        ":" +
+        now.getMinutes().toString().padStart(2, "0");
+
+      tasks.forEach((task) => {
+        if (
+          task.alarmTime &&
+          task.alarmTime.slice(0, 5) === currentTime
+        ) {
+          triggerAlarm(task.id);
+        }
+      });
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.interval);
+    this.interval = null;
+  }
+}
