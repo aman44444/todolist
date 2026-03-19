c46f5e111bb4b57857933351e5765ea42fe2e2fd
aman1357 <amancorp04@gmail.com>
2026-03-19 12:18:03 +0530

    feat: add AlarmPlayer

---
+++ src/Components/AlarmPlayer/AlarmPlayer.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useRef } from "react";
+import useTodosStore from "../../store/store";
+import alarmSound from "../../audio/alarm2.mp3";
+import "../../Styles/AlarmModal/AlarmModal.css";
+
+const AlarmPlayer = () => {
+  const { activeAlarmTaskId, mode, stopAlarm } = useTodosStore();
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    if (!audioRef.current) return;
+
+    if (activeAlarmTaskId) {
+      audioRef.current.loop = true;
+      audioRef.current.play().catch(() => {});
+
+      if (mode === "normal") {
+        setTimeout(() => {
+          handleStop();
+        }, 5000);
+      }
+    }
+  }, [activeAlarmTaskId, mode]);
+
+  const handleStop = () => {
+    if (!audioRef.current) return;
+
+    audioRef.current.pause();
+    audioRef.current.currentTime = 0;
+    stopAlarm();
+  };
+
+  return (
+    <>
+      <audio ref={audioRef} src={alarmSound} preload="auto" />
+
+      {mode === "strict" && activeAlarmTaskId && (
+        <div className="alarm-modal-overlay">
+          <div className="alarm-modal">
+            <h3>Alarm Ringing</h3>
+            <p>You must stop it manually</p>
+
+            <button onClick={handleStop} className="stop-btn">
+              Stop Alarm
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AlarmPlayer;
