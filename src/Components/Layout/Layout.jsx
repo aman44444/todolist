2dc986ade90db010e39127d5fdb53a9ba517f3d1
aman1357 <amancorp04@gmail.com>
2026-03-19 12:25:47 +0530

    add alarm modal in layout

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,15 +1,28 @@
+import { useEffect } from "react";
 import Autocomplete from "../Autocomplete/Autocomplete";
 import TasksContainer from "../TasksContainer/TaskContainer";
-import '../../Styles/Layout/Layout.css'
+import "../../Styles/Layout/Layout.css";
+import AlarmPlayer from "../AlarmPlayer/AlarmPlayer";
+import useTodosStore from "../../store/store";
+import { AlarmEngine } from "../../services/alarmEngine";
+
+const engine = new AlarmEngine();
 
 const Layout = () => {
-    return(
-        <div className="layout">
-              <Autocomplete/>
-              <TasksContainer/>
-        </div>
+  const { tasks, triggerAlarm } = useTodosStore();
 
-    )
-}
+  useEffect(() => {
+    engine.start(tasks, triggerAlarm);
+
+    return () => engine.stop();
+  }, [tasks, triggerAlarm]);
+  return (
+    <div className="layout">
+      <Autocomplete />
+      <AlarmPlayer />
+      <TasksContainer />
+    </div>
+  );
+};
 
 export default Layout;
